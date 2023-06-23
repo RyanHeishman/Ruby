a31959290b07fb435103a2592f5a55f02e15f7c6
RyanHeishman <heishy0@gmail.com>
2023-06-23 07:37:31 -0700

    Ruby Practice

---
+++ learning_to_program/chapter_6.rb
@@ -0,0 +1,85 @@
+puts 'Hello there, and what\'s your name?'
+name = gets.chomp
+puts 'Your name is ' + name + '? What a lovely name!'
+puts 'Pleased to meet you, ' + name + '. :)'
+
+puts 'What is your first name?'
+f_name = gets.chomp
+puts 'What is your middle name?'
+m_name = gets.chomp
+puts 'What is your last name?'
+l_name = gets.chomp
+puts 'Ah, nice to meet you ' + f_name + ' ' + m_name + ' ' + l_name
+
+puts 'What is your favorite number?'
+num = gets.chomp
+numm = num.to_i + 1
+puts numm.to_s
+puts 'I think this is a bigger and better number'
+
+var1 = 'hello'
+puts var1.reverse
+
+puts 'What is your first name?'
+fname = gets.chomp
+puts 'What is your middle name?'
+mname = gets.chomp
+puts 'What is your last name?'
+lname = gets.chomp
+sum = fname.length + mname.length + lname.length
+puts 'There are ' + sum.to_s + ' characters in your name'
+
+puts 'WHAT DO YOU WANT?!'
+need = gets.chomp
+puts 'WHADDAYA MEAN "' + need.upcase + '"?!? YOU\'RE FIRED'
+
+line_width = 30
+other = 60
+puts(                                  'Table of Contents'.center(other))
+puts " "
+puts("Chapter 1:     Getting Started".ljust(line_width) + "page 1".rjust(line_width))
+puts("Chapter 2:     Numbers".ljust(line_width) + "page 9".rjust(line_width))
+puts("Chapter 2:     Letters".ljust(line_width) + "page 13".rjust(line_width))
+
+puts 5**2
+puts 5**0.5
+puts 7/3
+puts 7%3
+puts 365/7.0
+
+puts (5-2).abs
+puts (2-5).abs
+
+puts rand
+puts rand
+puts rand
+puts rand(100)
+puts rand(100)
+puts rand(100)
+puts rand(1)
+puts rand(1)
+puts rand(1)
+puts rand(9999999999999999)
+puts('The weather man said there is a')
+puts(rand(101).to_s + '% chance of rain')
+puts('but you can never trust a weatherman.')
+
+srand 1976
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts ' '
+srand 1976
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts(rand(100))
+puts ' '
+
+puts(Math::PI)
+puts(Math::E)
+puts(Math.cos(Math::PI/3))
+puts(Math.tan(Math::PI/4))
+puts(Math.log(Math::E**2))
+puts((1 + Math.sqrt(5))/2)
