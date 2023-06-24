639eff918eda5b86571ab12dc572db7319165ee3
RyanHeishman <heishy0@gmail.com>
2023-06-24 13:29:07 -0700

    ruby chapter 8

---
+++ learning_to_program/chapter_8.rb
@@ -0,0 +1,46 @@
+# names = ['LeBron','Melo','Tracy']
+# puts names
+# puts
+# puts names[0]
+# puts names[1]
+# puts names[2]
+
+# yo = []
+# yo[3]='Jeff'
+# yo[1]='Latty'
+# yo[2]='Carl'
+# yo[0]='Tom'
+# puts yo
+
+# foods = ['pizza','eggs','pb']
+# puts foods
+# puts
+# puts foods.to_s
+# puts foods.join(', ')
+# puts foods.join(' :) ')+ ' 8)'
+
+# 200.times do
+#     puts []
+# end
+
+# hey = ['LeBron',['Kobe','Shaq'], 49]
+# puts hey
+
+# favorites = []
+# favorites.push 'Mushroom'
+# favorites.push 'Cloud'
+# puts favorites[0]
+# puts favorites.last
+# puts favorites.length
+# puts favorites.pop
+# puts favorites
+# puts favorites.length
+
+# list = []
+# word = ''
+# puts 'Type as many words as you\'d like'f
+#     while word != 'stop'
+#         word = gets.chomp
+#         list.push word
+#         puts list.join(', ')
+#     end
