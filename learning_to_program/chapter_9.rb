36d7d24f6e52481c022c261e6e76714b10745f1f
RyanHeishman <heishy0@gmail.com>
2023-06-26 12:18:17 -0700

    ruby chapter 9

--- learning_to_program/chapter_9.rb
+++ learning_to_program/chapter_9.rb
@@ -95,11 +95,95 @@
 
 # double_this 5
 
-def little_pest tough_var
-    tough_var = nil
-    puts 'HAHA! I ruined your variable!'
-end
+# def little_pest tough_var
+#     tough_var = nil
+#     puts 'HAHA! I ruined your variable!'
+# end
 
-tough_var = 'You can\'t even touch my variable!'
-little_pest tough_var
-puts tough_var
+# tough_var = 'You can\'t even touch my variable!'
+# little_pest tough_var
+# puts tough_var
+
+# return_val = puts 'This puts returned:'
+# puts return_val 
+
+# def say_moo number_of_moos
+#     puts 'moo ' * number_of_moos
+#     'yellow submarine'
+# end
+
+# x = say_moo 3
+# puts x.capitalize + ', dude...'
+# puts x
+
+# def favorite_food name
+#     if name == 'Lister'
+#         return 'vindaloo'
+#     end
+    
+#     if name == 'Rimmer'
+#         return 'mashed potatoes'
+#     end
+
+#     'hard to say...maybe fried plantain?'
+# end
+
+# def favorite_drink name
+#     if name == 'Jean-luc'
+#         'tea, Earl Grey, hot'
+#     elsif name == 'Kathryn'
+#         'coffee, black'
+#     else
+#         'perhaps...horchata?'
+#     end
+# end
+
+# puts favorite_food('Lister')
+# puts favorite_food('Rimmer')
+# puts favorite_drink('Jean-luc')
+# puts favorite_drink('Kathryn')
+# puts favorite_food('Carl')
+# puts favorite_drink('Carl')
+
+# def ask question
+#     while 
+#         puts question
+#         reply = gets.chomp.downcase
+
+#         if reply == 'yes'
+#             return true
+#         elsif reply == 'no'
+#             return false
+#         else
+#             puts 'Please answer "yes" or "no".'
+#         end
+#     end
+# end
+# puts 'Hello, and thank you for...'
+# ask ('Do you like eating tacos?')
+# ask ('Do you like eating Burritos?')
+# wets_bed = ask ('Do you wet the bed?')
+# ask ('Do you like eating chimichangas?')
+# ask ('Do you like eating sopapillas?')
+# ask ('Do you like drinking horchatas?')
+# ask ('Do you like eating flautas?')
+
+# puts
+# puts 'DEBRIEFING'
+# puts
+# puts wets_bed
+
+# def old_roman_numeral num
+#     if num.to_i <= 4 or num.to_i >= 0
+#         res = 'Roman Numeral: ' + 'I'*num.to_i
+#         puts res
+#     elsif num.to_i <= 10 or num.to_i >= 5
+#         res = 'Roman Numeral: ' + 'V' +'I'*num.to_i
+#         puts res
+#     else return nil
+#     end
+# end
+
+# puts 'Give Me a Number'
+# old_roman_numeral 5
+
